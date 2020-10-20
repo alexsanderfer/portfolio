de51a60d7f51e0730d734927b39fd98ef2ff32f7
alexsanderfer <afgouveia90@gmail.com>
2020-10-20 18:19:05 +0100

    Arquivos adicionados

---
+++ README.md
@@ -0,0 +1,45 @@
+# 🍣 Meu Portfólio<br>
+
+## 🖥 Visualização
+![Resultado final do projeto](image/resultado.jpg)
+
+## 🌳 Projeto<br>
+Portfólio criado com o intuito de mostrar meus recentes e futuros projetos ao decorrer da minha carreira.<br>
+
+## 📚 Seções
+O site é composto por 5 seções diferentes:
+
+- **Seção Header:** Nele temos um avatar do meu github e um breve apresentação;
+- **Seção Sobre Mim:** Nessa seção tenho um apresentação de um avatar só que andando e uma descrição dizendo sobre mim;
+- **Seção Projetos:** Apresenta alguns projetos desenvolvidos e com link direto para os respectivos códigos no GitHub;
+- **Seção Serviços:** Exibe através de cards os diferentes serviços em que possuo conhecimentos;
+- **Seção Conhecimentos:** Nele apresentamos meus conhecimentos em algumas linguagens como o foco no front-end;
+
+![Telas](image/ConjuntoTelas.png)
+
+## 🛠 Tecnologias utilizadas
+Para o desenvolvimento deste site utilizei as seguintes tecnologias:
+- Visual Studio Code;
+- HTML5;
+- JavaScript;
+- SASS (e convertido para CSS3);
+- JQuery;
+
+## 📌 Ajustes e melhorias
+O site ainda está em desenvolvimento e as próximas atualizações serão voltadas nas seguintes tarefas:
+
+(Próxima atualização: 17/10/2020)
+
+- [x] Adicionar Dark Mode (tema escuro); 
+- [x] Efeito flutuante;
+- [x] Animação no menu;  
+- [ ] Responsividade; 
+---
+
+## 🌈 Seja um dos contribuidores<br>
+Quer fazer parte desse projeto? É simples!<br>
+Faço um Fork nele colocando suas modificações e depois mande uma solicitação.
+
+<p align= center>
+🏰 Orgulhosamente feito por <strong>Iuri Silva</strong>
+</p>
---
+++ js/script.js
@@ -0,0 +1,117 @@
+//Animação menu responsivo
+$('.btn-menu, .btn-close').on('click', function () {
+    $('.menu').slideToggle('slow');
+});
+
+//Scroll menu
+$('nav a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top;
+
+    $('html, body').animate(
+        {
+            scrollTop: targetOffset,
+        },
+        1000
+    );
+});
+
+//Scroll top
+$(document).ready(function () {
+    var scrollTop = $('.scrollTop');
+
+    $(window).scroll(function () {
+        var topPos = $(this).scrollTop();
+
+        //quantidade Y que o botão de scroll vai aparecer
+        if (topPos > 500) {
+            $(scrollTop).css('opacity', '1');
+        } else {
+            $(scrollTop).css('opacity', '0');
+        }
+    });
+    //velocidade a animação
+    $(scrollTop).click(function () {
+        $('html, body').animate(
+            {
+                scrollTop: 0,
+            },
+            1000
+        );
+        return false;
+    });
+    //Links dos meus conhecimentos
+    const techs = [{
+        name: 'html5',
+        url: 'https://html5.org/'
+    }, {
+        name: 'css',
+        url: 'https://developer.mozilla.org/en-US/docs/Web/CSS'
+    }, {
+        name: 'js',
+        url: 'https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference',
+    }, {
+        name: 'sass',
+        url: 'https://sass-lang.com/'
+    }, {
+        name: 'bs',
+        url: 'https://getbootstrap.com/',
+    }, {
+        name: 'react',
+        url: 'https://pt-br.reactjs.org/',
+    }, {
+        name: 'materialize',
+        url: 'https://materializecss.com/',
+    }, {
+        name: 'ui',
+        url: 'https://figma.com/'
+    }]
+
+    techs.forEach(tech => {
+        $('.' + tech.name).on('click', () => {
+            window.open(tech.url)
+        })
+    });
+
+});
+
+//Dark Mode
+var darkMode;
+
+if (localStorage.getItem('dark-mode')) {
+    // if dark mode is not in storage, set variable to 'light'
+    darkMode = 'light';
+} else {
+    // if dark mode is in storage, set variable with that value
+    darkMode = localStorage.getItem('dark-mode');
+}
+
+// set new localStorage value
+localStorage.getItem('dark-mode', darkMode);
+
+if (localStorage.getItem('dark-mode') == 'dark') {
+    // if the above is 'dark' then apply .dark to the body
+    $('body').addClass('dark');
+    // hide the 'dark' button
+    $('.dark-button').hide();
+    // show the 'light' button
+    $('.light-button').show();
+}
+
+// Toggle dark UI
+$('.dark-button').on('click', function () {
+    $('.dark-button').hide();
+    $('.light-button').show();
+    $('body').addClass('dark');
+    // set stored value to 'dark'
+    localStorage.setItem('dark-mode', 'dark');
+});
+
+$('.light-button').on('click', function () {
+    $('.light-button').hide();
+    $('.dark-button').show();
+    $('body').removeClass('dark');
+    // set stored value to 'light'
+    localStorage.setItem('dark-mode', 'light');
+});
