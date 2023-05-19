4e8042e7c7288f678f6c0224d027b71e854ea29f
Alexsander Fernandes <33939423+alexsanderfer@users.noreply.github.com>
2023-05-19 21:18:37 +0100

    Social Links

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { BsTwitter, BsInstagram, BsGithub } from "react-icons/bs";
 import { FaFacebookF } from 'react-icons/fa'
 
 
@@ -8,7 +8,7 @@ const SocialMedia = () => {
         <div className="app__social">
             <div>
                 <a href="https://github.com/alexsanderfer/" target="_blank" rel="noopener noreferrer">
-                    <BSGithub />
+                    <BsGithub />
                 </a>
             </div>
             <div>
