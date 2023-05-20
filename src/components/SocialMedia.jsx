59665bc81e58c558f2ce68c8069d6b7898640cf5
Alexsander Fernandes <33939423+alexsanderfer@users.noreply.github.com>
2023-05-20 03:21:54 +0100

    Update SocialMedia.jsx
    Linkedin Added

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { BsTwitter, BsInstagram, BsGithub } from "react-icons/bs";
+import { BsTwitter, BsInstagram, BsGithub, BsLinkedin } from "react-icons/bs";
 import { FaFacebookF } from 'react-icons/fa'
 
 
 const SocialMedia = () => {
     return (
         <div className="app__social">
+        <div>
+                <a href="https://www.linkedin.com/in/alexsanderfer/" target="_blank" rel="noopener noreferrer">
+                    <BsLinkedin />
+                </a>
+            </div>
             <div>
                 <a href="https://github.com/alexsanderfer/" target="_blank" rel="noopener noreferrer">
                     <BsGithub />
