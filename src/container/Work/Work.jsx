aeda69fe7861e190e002e483039bdb29286d2996
Alexsander Fernandes <33939423+alexsanderfer@users.noreply.github.com>
2024-08-06 12:47:21 +0100

    Update Work.jsx

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -54,7 +54,7 @@ const Work = () => {
                 {filterWork.map((work, index) => (
                     <div className='app__work-item app__flex' key={index}>
                         <div className="app__work-img app__flex">
-                            <img alt="" src={urlFor(work.imgUrl)} alt={work.name}/>
+                            <img src={urlFor(work.imgUrl)} alt={work.name}/>
 
                             <motion.div
                                 whileHover={{opacity: [0, 1]}}
